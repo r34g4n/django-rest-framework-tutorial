e60f73433eee99355f0fdfe5ba23929943f7d381
G0r R34g4n <gorreagan@gmail.com>
2020-06-05 17:57:35 -0400

    Working with Serializers

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -5,7 +5,7 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 class SnippetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    code = serializers.CharField(styles={'base_template': 'textarea.html'})
+    code = serializers.CharField(style={'base_template': 'textarea.html'})
     linenos = serializers.BooleanField(required=False)
     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
