c1fa5587b0b2c3b0314e7a66917e5c9527df93ba
G0r R34g4n <gorreagan@gmail.com>
2020-06-08 10:05:43 -0400

    squashing bugs...

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_framework.urlspatterns import format_suffix_patterns
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 
