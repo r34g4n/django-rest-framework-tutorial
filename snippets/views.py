b522ac7a291c9bf0f451bce7432fe5d2550bba57
G0r R34g4n <gorreagan@gmail.com>
2020-06-09 14:11:14 -0400

    Creating an endpoint for the root of our API + Creating an endpoint for the highlighted snippets

--- snippets/views.py
+++ snippets/views.py
@@ -1,11 +1,22 @@
 from django.contrib.auth.models import User
 
+from rest_framework import renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from rest_framework import permissions
 from rest_framework import generics
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=None),
+        'snippets': reverse('snippet-list', request=request, format=None),
+    })
 
 
 
@@ -34,7 +45,7 @@ class SnippetList(generics.ListCreateAPIView):
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Retrieve, update or delete a snippet instance.
+    Retrieve, `update` or `delete` a snippet instance.
     """
 
     queryset = Snippet.objects.all()
@@ -43,4 +54,11 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
         permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly
     ]
 
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
