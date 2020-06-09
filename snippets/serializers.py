247e2176f3f0071a149a9b7ff291631135559c43
G0r R34g4n <gorreagan@gmail.com>
2020-06-09 12:50:12 -0400

    Adding endpoints for our User models

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,5 +1,16 @@
+from django.contrib.auth.models import User
+
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
+
 
 
 class SnippetSerializer(serializers.ModelSerializer):
@@ -9,3 +20,4 @@ class SnippetSerializer(serializers.ModelSerializer):
         fields = (
             'id', 'title', 'code', 'linenos', 'language', 'style'
         )
+
